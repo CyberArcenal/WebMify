70c938dbffb05cc9ea32e455cc17b3de73c34805
CyberArcenal <148480765+CyberArcenal@users.noreply.github.com>
2025-06-26 13:48:30 +0800

    Update global.js

--- src/js/api/global.js
+++ src/js/api/global.js
@@ -1,5 +1,6 @@
 export function global_base_url(){
     const url_1 = "http://127.0.0.1:8000"
     const url_2 = "https://fortfollio-system.vercel.app/";
-    return url_2;
+    const url_3 = "https://fortfollio-system.onrender.com/";
+    return url_3;
 };
